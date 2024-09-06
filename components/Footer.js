bebd5d5c9841800b32d1069beed653ba6cb3ec88
gr22nist <gr22nist@gmail.com>
2024-09-06 23:58:51 +0900

    🧩 전역 레이아웃 컴포넌트 (네비게이션, 푸터) 추가

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+
+  const container = `flex justify-center items-center`;
+
+  return (
+    <footer>
+      <section className={container}>
+        <p>© 2024 My력서</p>
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ components/NavBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/link";
+
+const NavBar = () => {
+
+  const container = `border-b`;
+  const navWrap = `container max-w-default mx-auto flex justify-between py-4`;
+  const navList = `flex gap-6`;
+
+  return (
+    <header className={container}>
+      <nav className={navWrap}>
+        <Link href="/" passHref>
+            마이력서
+        </Link> 
+        <ul className={navList}>
+          <li>
+            <Link href="/resume" passHref>
+                이력서
+            </Link>  
+          </li>
+          <li>
+            <Link href="/coverletter" passHref>
+                자기소개서
+            </Link>  
+          </li>
+          <li>
+            <Link href="/about" passHref>
+                서비스 소개
+            </Link>  
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
