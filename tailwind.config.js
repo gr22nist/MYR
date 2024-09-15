e4dfe4f0bdedfcda23e02ab84dbe0ab3c4e43470
gr22nist <gr22nist@gmail.com>
2024-09-15 22:43:14 +0900

    🔧 설정파일 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      maxWidth: {
+        "default": "1200px",
+      },
     },
   },
   plugins: [],
