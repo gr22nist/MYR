b19e9e745e74b4330e583be1cc37b22fe6acb3ae
gr22nist <gr22nist@gmail.com>
2024-09-17 00:24:25 +0900

    🎨  토스트 컴포넌트 수정

--- components/common/ToastComponent.js
+++ components/common/ToastComponent.js
@@ -1,22 +1,22 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { hideToast } from '@/redux/slices/uiSlice'; // hideToast 액션
+import { hideToast } from '@/redux/slices/globalSlice';
 
 const ToastComponent = () => {
   const dispatch = useDispatch();
-  const { toastMessage, toastType, toastVisible } = useSelector((state) => state.ui);
+  const { toastMessage, toastType, toastVisible } = useSelector((state) => state.global);
 
   useEffect(() => {
     if (toastVisible) {
       const timer = setTimeout(() => {
         dispatch(hideToast()); // 3초 후에 토스트를 숨김
-      }, 3000); // 3초로 설정 (원한다면 5000으로 변경 가능)
-
-      return () => clearTimeout(timer); // 컴포넌트가 언마운트될 때 타이머를 정리
+      }, 3000); // 3초 후 토스트 자동 숨김
+      
+      return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
     }
   }, [toastVisible, dispatch]);
 
-  if (!toastVisible) return null; // 토스트가 안 보일 때는 아무것도 렌더링하지 않음
+  if (!toastVisible) return null; // 토스트가 보이지 않을 때는 렌더링하지 않음
 
   return (
     <div className={`toast toast-${toastType} fixed bottom-4 right-4 p-4 bg-gray-800 text-white`}>
