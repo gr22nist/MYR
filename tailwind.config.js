63743b7995821daa295d26c90b3f72d69dc1aa78
gr22nist <gr22nist@gmail.com>
2024-09-24 23:28:52 +0900

    💄 테일윈드 커스텀 코드 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,45 @@
+import colors from './styles/colors';
+import { fonts } from './utils/fonts';  // fonts 임포트 추가
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      ...colors,
+      background: "var(--background)",
+      foreground: "var(--foreground)",
+    },
+    fontFamily: {
+      sans: ['var(--font-nanum)', 'system-ui', 'sans-serif'],
+    },
+    width: {
+      "myr": "1144px",
+    },
+    maxWidth: {
+      "myr": "1240px",
+    },
+    transitionDuration: {
+      '400': '400ms',
+    },
+    keyframes: {
+      itemEnter: {
+        '0%': { opacity: '0', transform: 'translateY(-20px)' },
+        '100%': { opacity: '1', transform: 'translateY(0)' },
       },
-      maxWidth: {
-        "default": "1200px",
+      itemExit: {
+        '0%': { opacity: '1', transform: 'translateY(0)' },
+        '100%': { opacity: '0', transform: 'translateY(-20px)' },
       },
     },
+    animation: {
+      'item-enter': 'itemEnter 300ms ease-out',
+      'item-exit': 'itemExit 300ms ease-in',
+    },
   },
-  plugins: [],
 };
+export const plugins = [];
