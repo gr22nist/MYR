2bff1c851abe07965fc35f4057e402f10a62e5a1
gr22nist <gr22nist@gmail.com>
2024-09-29 18:32:17 +0900

    ✋ 드래그 핸들 컴포넌트 구현

--- components/common/actions/DragHandleComponent.js
+++ components/common/actions/DragHandleComponent.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { useDraggable } from '@dnd-kit/core';
 import { CSS } from '@dnd-kit/utilities';
-import { DragHandleBtn, DeleteLineBtn } from '@/components/icons/IconSet'
+import { DragHandleBtn, DragHandleLineBtn } from '@/components/icons/IconSet'
 
-const DragHandleComponent = (props) => {
+const DragHandleComponent = ({ id, isSubItem, dragHandleProps }) => {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: props.id,
+    id: id,
   });
 
   const style = {
     transform: CSS.Translate.toString(transform),
   };
 
+  const handleProps = dragHandleProps || { ...listeners, ...attributes };
+
   return (
-    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
-      {/* 드래그 핸들 내용 */}
-      <DragHandleBtn />
+    <div ref={setNodeRef} style={style} {...handleProps} className="cursor-grab active:cursor-grabbing">
+      {isSubItem ? <DragHandleBtn /> : <DragHandleLineBtn />}
     </div>
   );
 };
