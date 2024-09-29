93a8fdfa9df802155d300e2e12d6b2c22cfc0e46
gr22nist <gr22nist@gmail.com>
2024-09-29 18:15:56 +0900

    🎨 전역 폰트 설정 개선 (NanumSquareNeo 적용)

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { initializeDB } from '@/hooks/dbConfig';
 import Layout from './layout';
 import '../styles/globals.css';
+import '../styles/modal.css';
+import ReactModal from 'react-modal';
+import localFont from 'next/font/local';
+
+const myFont = localFont({
+  src: [
+    {
+      path: '../public/fonts/NanumSquareNeo-Variable.woff2',
+      weight: '300 400 700 800 900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-nanum'
+});
+
+ReactModal.setAppElement('#__next');
 
 function MYR ({ Component, pageProps }) {
+  useEffect(() => {
+    initializeDB().catch(console.error);
+  }, []);
 
   return (
-    <Layout>
+    <Layout className={`${myFont.variable} font-sans`}>
       <Component {...pageProps} />
     </Layout>
   );
