c87f80cfc9dfbe8bcc616a23b9fbe028606eb516
gr22nist <gr22nist@gmail.com>
2024-09-30 11:30:31 +0900

    🔧 테일윈드 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export const content = [
   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   "./components/**/*.{js,ts,jsx,tsx,mdx}",
   "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  './styles/**/*.js',
 ];
 export const theme = {
   extend: {
