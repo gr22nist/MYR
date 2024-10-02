2d7f025b1c9e568baa00fa6c345d53eb104c758a
gr22nist <gr22nist@gmail.com>
2024-10-02 15:30:23 +0900

    🔒 보안 헤더 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,30 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data:;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // 모든 경로에 대해 헤더 적용
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
