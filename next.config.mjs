ccb393e565e4a9846a454074e464dfc813e9d50e
gr22nist <gr22nist@gmail.com>
2024-10-04 23:59:44 +0900

    🔒 다음 우편번호 API에 대한 CSP 설정 수정 및 보안 강화

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com https://cdn.jsdelivr.net;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com https://cdn.jsdelivr.net https://t1.daumcdn.net;
   style-src 'self' 'unsafe-inline';
-  img-src 'self' data: blob:;
+  img-src 'self' data: blob: https://t1.daumcdn.net;
   font-src 'self';
-  connect-src 'self' https://va.vercel-scripts.com;
+  connect-src 'self' https://va.vercel-scripts.com https://t1.daumcdn.net;
   worker-src 'self' blob:;
+  frame-src 'self' https://t1.daumcdn.net http://postcode.map.daum.net;
 `;
 
 const securityHeaders = [
