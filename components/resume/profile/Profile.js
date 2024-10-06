f89d5887b1cf58ad900711eeaa42dd3da627ea80
gr22nist <gr22nist@gmail.com>
2024-10-06 19:49:43 +0900

    🎨 프로필/사용자정보 폰트 오류 수정

--- components/resume/profile/Profile.js
+++ components/resume/profile/Profile.js
@@ -40,14 +40,11 @@ const Profile = () => {
 
 	const imageUrl = profile?.imageUrl || '';
 
-	const textAreaBaseStyle = 'p-4 bg-mono-f5 leading-normal text-mono-11 resize-none border-0 rounded-lg overflow-hidden';
-	const textAreaStyle = `${textAreaBaseStyle} ${common.focusStyle}`;
-
 	return (
-		<section className="flex flex-col gap-2">
-			<div className="relative w-full flex flex-row justify-between items-center gap-4">
+		<section className="profile-container">
+			<div className="profile-title">
 				<textarea
-					className={`${textAreaStyle} lg:text-4xl text-3xl font-extrabold h-36 flex-grow`}
+					className='profile-text-area-title leading-normal'
 					value={profile.title || ''}
 					onChange={(e) => handleChange('title', e.target.value)}
 					placeholder={`간단한 제목을 쓰거나 인사를 해주세요.\n두 줄로 쓰는 것이 가장 보기에 좋습니다.`}
@@ -56,7 +53,7 @@ const Profile = () => {
 			</div>
 			<textarea
         ref={textareaRef}
-        className='profile-text-area-paragraph'
+        className='profile-text-area-paragraph flex self-center'
         value={profile.paragraph || ''}
 				rows={3}
         onChange={(e) => {
@@ -64,7 +61,6 @@ const Profile = () => {
           autoResize();
         }}
         placeholder={`이력서는 읽은 지 10초 이내에 첫인상이 결정된다고 합니다.\n나를 나타내는 간결하고 멋진 슬로건과 입사포부, 나의 특장점 등.\n그것이 무엇이든 가장 당신다운 것을 이 곳에 작성해보세요.`}
-        spellCheck="false"
       />
 		</section>
 	);
