1e1df9ab2229080e216b06fd27f944363ba28a27
gr22nist <gr22nist@gmail.com>
2024-10-18 23:31:00 +0900

    🐛 주소 인풋 로직 개선

--- components/resume/userInfo/inputs/AddressInput.js
+++ components/resume/userInfo/inputs/AddressInput.js
@@ -1,14 +1,25 @@
-import React, { useCallback } from 'react';
+import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 
 const DaumPostcode = dynamic(() => import('react-daum-postcode'), { ssr: false });
 
-const AddressInput = ({ onChange, onClose }) => {
-  const handleComplete = useCallback((data) => {
-    const fullAddress = data.address;
-    onChange(fullAddress);
-    onClose();
-  }, [onChange, onClose]);
+const AddressInput = ({ onChange }) => {
+  const [address, setAddress] = useState('');
+
+  const handleComplete = (data) => {
+    const isRoadAddress = data.roadAddress !== '';
+    const filteredAddress = isRoadAddress
+      ? `${data.sido} ${data.sigungu} ${data.roadname}`
+      : `${data.sido} ${data.sigungu} ${data.bname}`;
+
+    setAddress(filteredAddress);
+    onChange(filteredAddress);
+  };
+
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+    onChange(e.target.value);
+  };
 
   return (
     <div>
