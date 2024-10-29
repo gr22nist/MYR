bcf1ffad1d742419ac189d7b3e34517723b367f5
gr22nist <gr22nist@gmail.com>
2024-10-29 15:47:48 +0900

    🚧 CLS 개선 중

--- components/resume/custom/CustomForm.js
+++ components/resume/custom/CustomForm.js
@@ -1,14 +1,12 @@
 import React, { useMemo, useCallback } from 'react';
 import TagButtons from '@/components/common/TagButtons';
 import { PREDEFINED_SECTIONS, CUSTOM_SECTIONS } from '@/constants/resumeConstants';
-import useResumeStore from '@/store/resumeStore';
 import usecustomStore from '@/store/customStore';
 import useResumeSections from '@/hooks/useResumeSections';
 
 const UNIQUE_SECTION_TYPES = ['project', 'award', 'certificate', 'language', 'skill', 'link'];
 
-const CustomForm = ({ onAddSection }) => {
-  const { sections } = useResumeStore();
+const CustomForm = React.memo(() => {
   const { addCustomSection, predefinedSections } = usecustomStore();
   const { loadAllSections } = useResumeSections();
 
@@ -17,33 +15,30 @@ const CustomForm = ({ onAddSection }) => {
     []
   );
 
-  const disabledTags = useMemo(() => {
-    return Object.keys(predefinedSections);
-  }, [predefinedSections]);
+  const disabledTags = useMemo(() => 
+    Object.keys(predefinedSections), 
+    [predefinedSections]
+  );
 
-  const canAddSection = useCallback((type) => {
-    return !UNIQUE_SECTION_TYPES.includes(type) || !predefinedSections[type];
-  }, [predefinedSections]);
-  
   const handleAddSection = useCallback(async (type) => {
-    if (!canAddSection(type)) {
+    if (UNIQUE_SECTION_TYPES.includes(type) && predefinedSections[type]) {
       alert('이 섹션은 이미 추가되어 있습니다.');
       return;
     }
-    const newSection = addCustomSection(type);
+    await addCustomSection(type);
     await loadAllSections();
-  }, [canAddSection, addCustomSection, loadAllSections]);
+  }, [predefinedSections, addCustomSection, loadAllSections]);
 
   return (
     <div className='controls-container'>
-      <div className='info-message text-small'>
+      <div className='info-message text-small cls'>
         작성하고 싶은 정보가 있는데 필드가 없나요? 직접 만들어보세요.
         저서, 논문, 봉사 경험, 교육활동 등 무엇이든 간단한 텍스트로 작성 가능합니다.
       </div>
       <div className='controls'>
         <button 
           onClick={() => handleAddSection(CUSTOM_SECTIONS.type)}
-          className='controls-custom-btn'
+          className='controls-custom-btn cls'
         >
           + 자유 서식 추가하기
         </button>
@@ -57,6 +52,8 @@ const CustomForm = ({ onAddSection }) => {
       </div>
     </div>
   );
-};
+});
 
-export default React.memo(CustomForm);
+CustomForm.displayName = 'CustomForm';
+
+export default CustomForm;
